05000cd78fc1c1c2599e0de39d21ce52addfefc0
Jemerylim <limjeremy16@gmail.com>
2024-10-21 03:36:34 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { AnimatePresence } from 'framer-motion';
 import { lazy, Suspense } from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom'; // Switch to HashRouter
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'; // Use HashRouter
 import ScrollToTop from './components/ScrollToTop';
 import AppFooter from './components/shared/AppFooter';
 import AppHeader from './components/shared/AppHeader';
@@ -14,27 +14,27 @@ const Projects = lazy(() => import('./pages/Projects'));
 const ProjectSingle = lazy(() => import('./pages/ProjectSingle.jsx'));
 
 function App() {
-	return (
-		<AnimatePresence>
-			<div className="bg-secondary-light dark:bg-primary-dark transition duration-300">
-				<Router>
-					<ScrollToTop />
-					<AppHeader />
-					<Suspense fallback={""}>
-						<Routes>
-							<Route path="/" element={<Home />} />
-							<Route path="projects" element={<Projects />} />
-							<Route path="projects/:id" element={<ProjectSingle />} />
-							<Route path="about" element={<About />} />
-							<Route path="contact" element={<Contact />} />
-						</Routes>
-					</Suspense>
-					<AppFooter />
-				</Router>
-				<UseScrollToTop />
-			</div>
-		</AnimatePresence>
-	);
+    return (
+        <AnimatePresence>
+            <div className=" bg-secondary-light dark:bg-primary-dark transition duration-300">
+                <Router>
+                    <ScrollToTop />
+                    <AppHeader />
+                    <Suspense fallback={""}>
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="projects" element={<Projects />} />
+                            <Route path="projects/:id" element={<ProjectSingle />} />
+                            <Route path="about" element={<About />} />
+                            <Route path="contact" element={<Contact />} />
+                        </Routes>
+                    </Suspense>
+                    <AppFooter />
+                </Router>
+                <UseScrollToTop />
+            </div>
+        </AnimatePresence>
+    );
 }
 
 export default App;
